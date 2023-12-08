5bcf6d55f30f24d720e7ee9ce498d1846e0f23fe
Rendell Padu <rendell.padu02@gmail.com>
2023-12-08 06:34:00 -0500

    both scripts working

--- Kindle_Review/script.js
+++ Kindle_Review/script.js
@@ -1,145 +1,168 @@
-document.addEventListener("DOMContentLoaded", function () {
-  function runQuery(endpoint, resultDivId) {
-    const getDataUrl = `http://127.0.0.1:5000/${endpoint}`;
 
-    axios
-      .get(getDataUrl)
-      .then(function (response) {
-        console.log("Data fetched successfully:", response.data);
+// Function to create Line Chart
+function createLineGraph(result) {
 
-        const result = response.data.result;
+  const labels = [];
+  const dataValues1 = []; // Price 1
+  const dataValues2 = []; // Price 2
 
-        // Display the result in the respective query result div
-        if (Array.isArray(result)) {
-          document.getElementById(resultDivId).innerHTML = JSON.stringify(
-            result,
-            null,
-            2
-          );
-          if (resultDivId === "query3Result") {
-            createPieChart(result); // Call function to create pie chart
-          } else if (resultDivId === "query2Result") {
-            createBarGraph(result); // Call function to create bar graph
-          }
-        } else {
-          console.error("Data received is not an array:", result);
-          document.getElementById(resultDivId).innerHTML =
-            "Error: Data is not in the expected format";
-        }
-      })
-      .catch(function (error) {
-        console.error("Error fetching data:", error);
-        document.getElementById(resultDivId).innerHTML = "Error fetching data";
-      });
-  }
+  // Process data for line chart (sample implementation)
 
-  // Function to create a pie chart
-  function createPieChart(result) {
-    const ctxQuery1 = document.getElementById("chartQuery1").getContext("2d");
 
-    const labels = ["Highly Satisfied", "Satisfied", "Neutral", "Unsatisfied"];
-    const dataValues = [];
-
-    // Assuming the result contains counts for the four categories
-    if (Array.isArray(result) && result.length > 0) {
-      dataValues.push(result[0].HighlySatisfied);
-      dataValues.push(result[0].Satisfied);
-      dataValues.push(result[0].Neutral);
-      dataValues.push(result[0].Unsatisfied);
-    } else {
-      console.error("Invalid or empty result");
-      document.getElementById("query3Result").innerHTML =
-        "Error: Invalid or empty data";
-      return;
-    }
-    j;
-
-    const myPieChart = new Chart(ctx, {
-      type: "pie",
-      data: {
-        labels: labels,
-        datasets: [
-          {
-            label: "Customer Segmentation",
-            data: dataValues,
-            backgroundColor: [
-              "rgba(255, 99, 132, 0.7)",
-              "rgba(54, 162, 235, 0.7)",
-              "rgba(255, 206, 86, 0.7)",
-              "rgba(75, 192, 192, 0.7)",
-              // Add more colors if needed
-            ],
-            borderWidth: 1,
-          },
-        ],
-      },
-      options: {
-        // Add options for the pie chart as needed
-      },
+  // Check if the result is valid and not empty
+  if (Array.isArray(result) && result.length > 0) {
+    result.forEach(item => {
+      labels.push(item._id);
+      dataValues1.push(item.launch_price);
+      dataValues2.push(item.price_after_90_days);
     });
+  } else {
+    console.error('Invalid or empty result');
+    document.getElementById('query1Result').innerHTML = 'Error: Invalid or empty data';
+    return;
   }
 
-  //Function to create Bar Graph
+  return {
+    labels: labels,
+    datasets: [{
+      label: 'Launch Price',
+      data: dataValues1,
+      borderColor: 'rgba(255, 99, 132, 1)', // Adjust color as needed
+      borderWidth: 1,
+      // fill: false
+    },
+    {
+      label: 'Price After 90 Days',
+      data: dataValues2,
+      borderColor: 'rgba(54, 162, 235, 1)', // Adjust color as needed
+      borderWidth: 1,
+      fill: false
+    }],
+  };
+}
+// Function to create a Bar Graph
+function createBarGraph(result) {
+  const labels = [];
+  const dataValues1 = [];
+  const dataValues2 = [];
 
-  function createBarGraph(result) {
-    const ctxQuery2 = document.getElementById("chartQuery2").getContext("2d");
-    // Check if the result is valid and not empty
-    if (Array.isArray(result) && result.length > 0) {
-      result.forEach((item) => {
-        labels.push(item._id); // Assuming the city is available in _id
-        dataValues1.push(item.average_helpful_votes);
-        dataValues2.push(item.average_rating);
-      });
-    } else {
-      console.error("Invalid or empty result");
-      document.getElementById("query2Result").innerHTML =
-        "Error: Invalid or empty data";
-      return;
-    }
-    // Process data for vertical bar chart (sample implementation)
-    const labels = [];
-    const dataValues1 = [];
-    const dataValues2 = [];
-    new Chart(ctxBar, {
-      type: "bar", // Change to 'bar' for vertical bar chart
-      data: {
-        labels: labels,
-        datasets: [
-          {
-            label: "No. of Helpful Votes",
-            data: dataValues1,
-            backgroundColor: "rgba(75, 192, 192, 0.7)", // Adjust color as needed
-            borderWidth: 1,
-          },
-          {
-            label: "No. of Ratings",
-            data: dataValues2,
-            backgroundColor: "rgba(100, 0, 0, 0.7)", // Adjust color as needed
-            borderWidth: 1,
-          },
-        ],
-      },
-      options: {
-        scales: {
-          x: {
-            beginAtZero: true,
-          },
-          y: {
-            beginAtZero: true,
-          },
-        },
-      },
+  // Check if the result is valid and not empty
+  if (Array.isArray(result) && result.length > 0) {
+    result.forEach((item) => {
+      labels.push(item._id); // Assuming the city is available in _id
+      dataValues1.push(item.average_helpful_votes);
+      dataValues2.push(item.average_rating);
     });
+  } else {
+    console.error("Invalid or empty result");
+    document.getElementById("query2Result").innerHTML =
+      "Error: Invalid or empty data";
+    return;
   }
-  document.getElementById("query1Btn").addEventListener("click", function () {
-    runQuery("task1", "query1Result");
+  return {
+    labels: labels,
+    datasets: [
+      {
+        label: "No. of Helpful Votes",
+        data: dataValues1,
+        backgroundColor: "rgba(75, 192, 192, 0.7)",
+        borderWidth: 1,
+      },
+      {
+        label: "No. of Ratings",
+        data: dataValues2,
+        backgroundColor: "rgba(100, 0, 0, 0.7)",
+        borderWidth: 1,
+      },
+    ],
+  };
+}
+// Function to create a Pie Graph
+function createPieChart(result) {
+
+  const labels = ["Highly Satisfied", "Satisfied", "Neutral", "Unsatisfied"];
+  const dataValues = [];
+
+  // Assuming the result contains counts for the four categories
+  if (Array.isArray(result) && result.length > 0) {
+    dataValues.push(result[0].HighlySatisfied);
+    dataValues.push(result[0].Satisfied);
+    dataValues.push(result[0].Neutral);
+    dataValues.push(result[0].Unsatisfied);
+  } else {
+    console.error("Invalid or empty result");
+    document.getElementById("query3Result").innerHTML =
+      "Error: Invalid or empty data";
+    return;
+  }
+
+
+  return {
+    labels: labels,
+    datasets: [
+      {
+        label: "Customer Segmentation",
+        data: dataValues,
+        backgroundColor: [
+          "rgba(255, 99, 132, 0.7)",
+          "rgba(54, 162, 235, 0.7)",
+          "rgba(255, 206, 86, 0.7)",
+          "rgba(75, 192, 192, 0.7)",
+          // Add more colors if needed
+        ],
+        borderWidth: 1,
+      },
+    ],
+  };
+}
+
+
+const makeAxiosCall = async (endpoint) => {
+  const getDataUrl = `http://127.0.0.1:5000/${endpoint}`;
+
+
+
+  const res = await axios.get(getDataUrl);
+
+  switch (endpoint) {
+    case "task1":
+      const retval = createLineGraph(res.data.result)
+      console.log(retval)
+      return retval
+
+    case "task2":
+      return createBarGraph(res.data.result)
+    case "task3":
+      return createPieChart(res.data.result)
+    default:
+      break;
+  }
+
+
+};
+
+
+const mainFunc = async () => {
+  console.log(await makeAxiosCall("task1"));
+  const ctxQuery1 = document.getElementById("chartQuery1").getContext("2d");
+  new Chart(ctxQuery1, {
+    type: 'line',
+    data: await makeAxiosCall("task1")
   });
 
-  document.getElementById("query2Btn").addEventListener("click", function () {
-    runQuery("task2", "query2Result");
+  const ctxQuery2 = document.getElementById("chartQuery2").getContext("2d");
+  new Chart(ctxQuery2, {
+    type: "bar",
+    data: await makeAxiosCall("task2")
   });
 
-  document.getElementById("query3Btn").addEventListener("click", function () {
-    runQuery("task3", "query3Result");
+  const ctxQuery3 = document.getElementById("chartQuery3").getContext("2d");
+  new Chart(ctxQuery3, {
+    type: "pie",
+    data: await makeAxiosCall("task3")
   });
+}
+
+["hashchange", "load"].forEach(event => {
+  window.addEventListener(event, mainFunc);
 });
